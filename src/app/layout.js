63c4769cb607102d5c80dcbd5deb7a3ff3286fba
lancellq <lancellq@gmail.com>
2026-07-16 17:31:42 -0500

    step: big change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,8 @@
 import './globals.css'
 import Navbar from '@/components/navbar/Navbar'
 import Footer from '@/components/footer/Footer'
-import { Inter, Roboto, Poppins } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/context/ThemeContext'
-// import {SessionProvider} from "next-auth/react";
 import AuthProvider from '@/components/AuthProvider/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
